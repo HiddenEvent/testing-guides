b28d43f3d6c2ee3d39db423c75e2ba3623af5a0d
RichardKim <hiddeneventor@gmail.com>
2023-11-24 22:55:46 +0900

    Initial commit

---
+++ src/main/java/me/ricky/guides/testingguides/TestingGuidesApplication.java
@@ -0,0 +1,13 @@
+package me.ricky.guides.testingguides;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingGuidesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestingGuidesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/ricky/guides/testingguides/TestingGuidesApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ricky.guides.testingguides;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingGuidesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
