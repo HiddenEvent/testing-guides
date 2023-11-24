44f7b8c4cd13898fac2923d19479881f338f92a9
RichardKim <hiddeneventor@gmail.com>
2023-11-25 00:09:02 +0900

    [BDD] 전체조회 테스트

--- src/test/java/me/ricky/guides/testingguides/repository/EmployeeRepositoryTest.java
+++ src/test/java/me/ricky/guides/testingguides/repository/EmployeeRepositoryTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+import java.util.Set;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 @DataJpaTest
@@ -12,7 +15,7 @@ class EmployeeRepositoryTest {
     @Autowired
     private EmployeeRepository employeeRepository;
     @Test
-    void 저장로직(){
+    void 저장(){
         //given
         Employee employee = Employee.builder()
                 .firstName("Ricky")
@@ -26,8 +29,29 @@ class EmployeeRepositoryTest {
         //then
         assertNotNull(savedEmployee);
         assertThat(savedEmployee.getId()).isGreaterThan(0);
-
-
     }
+    @Test
+    void 전체_조회() {
+        //given
+        Employee employee = Employee.builder()
+                .firstName("Ricky")
+                .lastName("Kim")
+                .email("aa@a.com")
+                .build();
 
+        Employee employee1 = Employee.builder()
+                .firstName("Ricky1")
+                .lastName("Kim1")
+                .email("aa1@a.com")
+                .build();
+        employeeRepository.save(employee);
+        employeeRepository.save(employee1);
+
+        //when
+        List<Employee> employees = employeeRepository.findAll();
+
+        //then
+        assertThat(employees).isNotNull();
+        assertThat(employees.size()).isEqualTo(2);
+    }
 }
