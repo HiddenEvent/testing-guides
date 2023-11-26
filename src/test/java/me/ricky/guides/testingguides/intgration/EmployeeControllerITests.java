2b50346dd1e041dbb077c72c9cc670de14554815
RichardKim <hiddeneventor@gmail.com>
2023-11-26 22:10:00 +0900

    [ControllerLayer] 통합테스트 기본 구성

---
+++ src/test/java/me/ricky/guides/testingguides/intgration/EmployeeControllerITests.java
@@ -0,0 +1,25 @@
+package me.ricky.guides.testingguides.intgration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import me.ricky.guides.testingguides.repository.EmployeeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class EmployeeControllerITests {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private EmployeeRepository employeeRepository;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setup() {
+        employeeRepository.deleteAll();
+    }
+}
