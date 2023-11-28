2a31905f5868c78ab24774908e7a71f931dcf366
RichardKim <hiddeneventor@gmail.com>
2023-11-29 00:36:43 +0900

    [Mongo] EmployeeDoc 모델 생성

---
+++ src/main/java/me/ricky/guides/testingguides/model/EmployeeDoc.java
@@ -0,0 +1,18 @@
+package me.ricky.guides.testingguides.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "employees")
+public class EmployeeDoc {
+    @Id
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+}
