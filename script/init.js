72d8866b05dc47da5f88ec78755d226e4b43c679
RichardKim <hiddeneventor@gmail.com>
2023-11-28 23:09:41 +0900

    mongoDB 환경 세팅

---
+++ script/init.js
@@ -0,0 +1,11 @@
+// 사용할 데이터베이스 선택
+db = db.getSiblingDB('testing');
+
+// 데이터베이스에 컬렉션 생성
+db.createCollection('employees');
+
+// 컬렉션에 초기 데이터 추가
+db.employees.insert({
+    _id: '1',
+    email: 'aa@a.com'
+});
